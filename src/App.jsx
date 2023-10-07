a46b94819b8a0b4a5874b47e31597e6ca976deef
Michael Lunzola <Michael>
2023-10-07 08:03:33 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+const App = () => {
+  return ( 
+      <div>
+        <img class="mx-auto my-auto pt-3" src="https://media.giphy.com/media/wNDa1OZtvl6Fi/giphy.gif" alt="ratatouille" />
+        <p className="max-w-2xl mx-auto mt-3 ">
+        Explicabo commodo illum veniam, eleifend labore nam minus lectus? Congue, 
+        tempor qui! Quos morbi pharetra placerat? Asperiores incidunt! Delectus 
+        cumque molestias illum dapibus? Deleniti aptent, iure sollicitudin quasi 
+        ratione dictum rutrum euismod, aut iusto, phasellus accusantium incididunt
+        magnis urna perspiciatis, sunt eum potenti eos! Perferendis asperiores,
+        adipiscing cillum? Mus neque.
+        Explicabo commodo illum veniam, eleifend labore nam minus lectus? Congue, 
+        tempor qui! Quos morbi pharetra placerat? Asperiores incidunt! Delectus 
+        cumque molestias illum dapibus? Deleniti aptent, iure sollicitudin quasi 
+        ratione dictum rutrum euismod, aut iusto, phasellus accusantium incididunt
+        magnis urna perspiciatis, sunt eum potenti eos! Perferendis asperiores,
+        adipiscing cillum? Mus neque.
+        </p>
+
+      </div>
+   );
+}
+ 
+export default App;
